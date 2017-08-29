ccfc4becb5716dda16dd13a714e59ecb142b9b8a
wenbo <wenbya@outlook.com>
2017-08-29 18:55:18 +0800

    test for eclipse and github

---
+++ src/elasticsearchJavaDemo/EsJavaDemo.java
@@ -0,0 +1,10 @@
+package elasticsearchJavaDemo;
+
+public class EsJavaDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I will show a es java api demo later in the repo ");
+	}
+
+}
